94e3ef36ec11379ec2ff80895cbae00971d51c08
cmartinezcsti <cmartinez@csticorp.biz>
2024-06-18 13:04:59 -0500

    New Front / Dashboard

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Elimina todas las variables de sesión
-session_destroy(); // Destruye la sesión
-
-// Redirige al usuario a la página de inicio u otra página después del logout
-header("Location: index");
-exit();
-?>
